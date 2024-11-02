d6b432391dd1a436947adf915e2999fad69e81c8
Seifeldin Abouelella <seifaldin.aboelella@gmail.com>
2024-11-02 13:49:09 +0100

    feat: init db and update node version

---
+++ mdbInit.js
@@ -0,0 +1,11 @@
+db = db.getSiblingDB("test")
+
+db.users.insert({
+	username: "admin",
+	password: "$2b$10$7WCXDJ56W9afRcXVtkOB5Ola0OCSDU93X7V9QPIfEK0psZmVb/FdS",
+})
+
+db.devices.insert({
+	deviceName: "Drone1",
+	deviceSchool: "School1",
+})
